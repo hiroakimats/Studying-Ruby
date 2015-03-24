b36e41c7300a2867c865bfc803ad16af099d851f
Matsunaga Hiroaki <hiroakimats@jp.sony.com>
2015-03-24 11:55:49 +0900

    ex2502完

---
+++ ex2502.rb
@@ -0,0 +1,30 @@
+require 'WEBrick'
+require 'date'
+
+
+config = {
+    :Port => 1020,
+    :DocumentRoot => '.'
+}
+
+server = WEBrick::HTTPServer.new(config)
+
+server.mount_proc("/testprog") {|req, res|
+  res.body << "<html><body><p>アクセスした日付は#{Date.today.to_s}です。</p>"
+  res.body << "<p>リクエストのパスは#{req.path}でした</p>"
+  res.body << "<ul>"
+  req.each{ |key, value|
+    res.body << "<li>#{key}:#{value}</li>"
+  }
+  res.body << "</ul>"
+  res.body << "</body></html>"
+}
+
+
+trap(:INT) do 
+  server.shutdown
+end
+
+
+server.start
+
